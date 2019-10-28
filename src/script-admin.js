a406a8e4677d71a16d81fedb25290cc4685eb792
Sam Zabala <samapitongzabala@gmail.com>
2019-10-28 09:06:40 -0700

    label + name size is cute now

--- src/script-admin.js
+++ src/script-admin.js
@@ -1,5 +1,5 @@
 /*!
-* 1point21 Data Vizualiser Version 1.2.0.2
+* 1point21 Data Vizualiser Version 2.0.0
 * Admin Script
 * DO NOT EDIT min.js
 * edit its corresponding unminified js file in /src instead
